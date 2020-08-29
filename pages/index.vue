af88403ee7ef4e823546b812c5c6fe4538a00a85
talog <tee.talog@gmail.com>
2020-08-29 22:08:24 +0900

    add update link

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="container">
     <div>
-      <nuxt-link to="/user/add" underline="false"><i class="el-icon-user-solid"></i>Add</nuxt-link>
+      <nuxt-link to="/user/add" underline="false"><i class="el-icon-user-solid"></i>追加</nuxt-link>
     </div>
 
     <div>
@@ -9,6 +9,11 @@
         <el-table-column prop="id" label="id" />
         <el-table-column prop="name" label="名前" />
         <el-table-column prop="biography" label="説明" />
+        <el-table-column label="">
+          <template v-slot="slotProps">
+            <el-button size="small" @click="moveToUpdateUser(slotProps.row.id)">編集</el-button>
+          </template>
+        </el-table-column>
       </el-table>
     </div>
   </div>
@@ -20,14 +25,18 @@ import { User } from '../types/data/user'
 import { userList } from '../api/user-list'
 
 export default defineComponent({
-  setup() {
+  setup(_props, context) {
     const users = ref<User[]>([])
 
     userList().then((res) => {
       users.value = res
     })
 
-    return { users }
+    const moveToUpdateUser = (id: string) => {
+      context.root.$router.push(`/user/${id}/update`)
+    }
+
+    return { users, moveToUpdateUser }
   },
 })
 </script>
